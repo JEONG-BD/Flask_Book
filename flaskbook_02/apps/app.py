0628331449e8e74ef788eda21e14cf81d19c8848
JEONG-BD <dev.byeongdu@gmail.com>
2023-11-05 15:06:48 +0900

    2023-11-05 sqlalchemy

--- flaskbook_02/apps/app.py
+++ flaskbook_02/apps/app.py
@@ -1,11 +1,23 @@
 from flask import Flask 
+from pathlib import Path 
+from flask_migrate import Migrate 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__) 
-
+    app.config.from_mapping(
+        SCRET_KEY = '2AZSMss3p05QpbcY2hBsJ',
+        SQLALCHEMY_DATABASE_URI = 
+            f"sqlite:///{Path(__file__).parent.parent / 'local.sqllite'}", 
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SQLALCHEMY_ECHO = True 
+    )
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
-
+    db.init_app(app)
+    Migrate(app, db)
     return app 
-    
+     
--- flaskbook_02/apps/crud/views.py
+++ flaskbook_02/apps/crud/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from apps.app import db 
+from apps.crud.models import User 
 
 crud = Blueprint(
     'crud', 
@@ -7,6 +9,17 @@ crud = Blueprint(
     static_folder = 'static', 
 )
 
+
 @crud.route('/')
 def index():
     return render_template('crud/index.html')
+
+
+@crud.route('/sql')
+def sql():
+    db.session.query(User).all()
+    return 'Check your console logs'
+
+
+
+
